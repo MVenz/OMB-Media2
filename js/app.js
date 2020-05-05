34a651c2f94ec7089e8d521f6922434dcfee69ae
Mario Vences <m.vences73@gmail.com>
2020-05-05 14:08:51 -0500

    OMB-Media2

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/*  |==============|
+    |   Typed Js   |
+    |==============|
+*/
+var options = {
+    strings: ["<i>Páginas WEB</i>", "<i>Landing Pages</i>", "<i>Micrositios Profesionales</i>"],
+    typeSpeed: 100,
+    backSpeed: 10,
+    backDelay: 10,
+    startDelay: 100,
+    loop: true,
+  }
+  var typed = new Typed(".typed", options);
